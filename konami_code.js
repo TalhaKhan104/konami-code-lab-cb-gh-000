9de377ce455c6ead21769ba4c0c83a32907832fe
Talha Khan <talha0128@gmail.com>
2019-01-01 23:09:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,17 @@ const codes = [
   "b",
   "a"
 ];
+let index = 0;
+function init(e) {
+  const key = e.key;
+  if(key === codes[index]){
+    index++;
+    if(key === codes.length){
+      alert('HURRAY');
+      index = 0;
 
-function init() {
-  // your code here
+    }
+  } else{
+    index = 0;
+  }
 }
